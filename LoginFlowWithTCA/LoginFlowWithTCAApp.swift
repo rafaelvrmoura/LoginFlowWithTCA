8b6fbfe76aea1d0e8a1a89934966bc169edc4464
Rafael Moura <rafael.vrmoura@gmail.com>
2025-10-11 19:05:31 -0300

    Initial Commit

---
+++ LoginFlowWithTCA/LoginFlowWithTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginFlowWithTCAApp.swift
+//  LoginFlowWithTCA
+//
+//  Created by Rafael Vieira Moura on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct LoginFlowWithTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
